ea2556b5541dc938519b4ceef34997ac9dca2109
EdSaezLo <edgar.saez.lopez@fxlluchirafecas.cat>
2024-03-28 14:07:20 +0100

    subir proyecto

---
+++ app/app/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'homepage/home.html')
+
+def login(request):
+    return render(request,'homepage/login.html')
